83672a474f2fd7327877fb7cf638ed7071dc0cb0
joeriddles <joeriddles10@gmail.com>
2018-10-04 15:58:35 -0700

    Assignment 2 Calculator Submission.

---
+++ Assignment2/src/Calculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public static void Main(string[] args)
+        {
+	        string userInput;
+
+	        if (args.Length > 1)
+	        {
+				Console.WriteLine($"Passed in expression: {String.Join(" ", args.Skip(1))}");
+		        userInput = String.Join(" ", args.Skip(1));
+	        }
+	        else
+	        {
+		        Console.WriteLine($"Welcome to Calculator!{Environment.NewLine}Enter a mathematical expression:{Environment.NewLine}Ex: 2 x 3");
+		        userInput = Console.ReadLine();
+			}
+
+			while (!userInput.Trim().Equals("n"))
+			{
+				bool validExpression = true;
+				var userInputArray = userInput.Split(" ");
+
+				if (userInputArray.Length != 3)
+					validExpression = InvalidExpression("Expression is not properly formatted.");
+				else
+				{
+					if (!Decimal.TryParse(userInputArray[0], out decimal leftOperand))
+						validExpression = InvalidExpression("Left operand is not a valid number.");
+
+					if (!Decimal.TryParse(userInputArray[2], out decimal rightOperand))
+						validExpression = InvalidExpression("Right operand is not a valid number.");
+
+					if (!TryParseOperator(userInputArray[1], out string expressionOperator))
+						validExpression = InvalidExpression("Passed in operator is not a valid operator.");
+
+					if (validExpression)
+						Console.WriteLine($"Answer: {SolveExpression(leftOperand, rightOperand, expressionOperator)}");
+				}
+
+				Console.WriteLine("Type 'n' to end program, type new expression to continue.");
+				userInput = Console.ReadLine();
+	        }
+
+        }
+
+        static decimal SolveExpression(decimal leftOperand, decimal rightOperand, string expressionOperator)
+        {
+	        switch (expressionOperator)
+	        {
+				case "+": return leftOperand + rightOperand;
+				case "-": return leftOperand - rightOperand;
+				case "*": return leftOperand * rightOperand;
+				case "x": return leftOperand * rightOperand;
+				case "/": return leftOperand / rightOperand;
+				case "**": return (decimal) Math.Pow((double) leftOperand, (double) rightOperand);
+				default: throw new ArgumentException("Passed in operator is not a valid operator.");
+			}
+        }
+
+        static bool TryParseOperator(string stringOperator, out string expressionOperator)
+        {
+	        if (new[] {"+", "-", "*", "/", "x", "**"}.Contains(stringOperator))
+	        {
+		        expressionOperator = stringOperator.Trim();
+		        return true;
+	        }
+
+	        expressionOperator = "";
+	        return false;
+        }
+
+	    static bool InvalidExpression(string consoleOutput)
+	    {
+			Console.WriteLine(consoleOutput);
+		    return false;
+	    }
+
+    }
+}
---
+++ Assignment2/test/CalculatorTest.cs
@@ -0,0 +1,93 @@
+using IntelliTect.TestTools.Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestCalculatorAddSucceed()
+        {
+			ConsoleAssert.Expect(GenerateConsoleOutputSucceed(3, 5, "+", 8), Calculator.Calculator.Main);
+        }
+
+	    [TestMethod]
+	    public void TestCalculatorSubtractSucceed()
+	    {
+		    ConsoleAssert.Expect(GenerateConsoleOutputSucceed(3, 5, "-", -2), Calculator.Calculator.Main);
+	    }
+
+	    [TestMethod]
+	    public void TestCalculatorMultiplySucceed()
+	    {
+		    ConsoleAssert.Expect(GenerateConsoleOutputSucceed(3, 5, "*", 15), Calculator.Calculator.Main);
+		}
+
+	    [TestMethod]
+	    public void TestCalculatorMultiplyAltSucceed()
+	    {
+		    ConsoleAssert.Expect(GenerateConsoleOutputSucceed(3, 5, "x", 15), Calculator.Calculator.Main);
+	    }
+
+		[TestMethod]
+	    public void TestCalculatorDivideSucceed()
+	    {
+		    ConsoleAssert.Expect(GenerateConsoleOutputSucceed(3, 5, "/", (decimal) 0.6), Calculator.Calculator.Main);
+		}
+
+	    [TestMethod]
+	    public void TestCalculatorPowSucceed()
+	    {
+		    ConsoleAssert.Expect(GenerateConsoleOutputSucceed(3, 5, "**", 243), Calculator.Calculator.Main);
+	    }
+
+		[TestMethod]
+	    public void TestCalculatorInvalidInputAll()
+	    {
+		    ConsoleAssert.Expect(GenerateConsoleOutputFail("fail", "Expression is not properly formatted."), Calculator.Calculator.Main);
+	    }
+
+	    [TestMethod]
+	    public void TestCalculatorInvalidInputLeftOperand()
+	    {
+		    ConsoleAssert.Expect(GenerateConsoleOutputFail("invalid + 1", "Left operand is not a valid number."), Calculator.Calculator.Main);
+	    }
+
+	    [TestMethod]
+	    public void TestCalculatorInvalidInputRightOperand()
+	    {
+		    ConsoleAssert.Expect(GenerateConsoleOutputFail("1 + invalid", "Right operand is not a valid number."), Calculator.Calculator.Main);
+	    }
+
+	    [TestMethod]
+	    public void TestCalculatorInvalidInputOperator()
+	    {
+		    ConsoleAssert.Expect(GenerateConsoleOutputFail("1 invalid 1", "Passed in operator is not a valid operator."), Calculator.Calculator.Main);
+	    }
+
+		private string GenerateConsoleOutputSucceed(decimal leftOperand, decimal rightOperand, string expressionOperator,
+		    decimal expectedOutput)
+	    {
+		    return $@"{initialConsoleOutput}
+<<{leftOperand} {expressionOperator} {rightOperand}
+>>Answer: {expectedOutput}
+{endConsoleOutput}";
+	    }
+
+	    private string GenerateConsoleOutputFail(string badInput, string failOutput)
+	    {
+			return $@"{initialConsoleOutput}
+<<{badInput}
+>>{failOutput}
+{endConsoleOutput}";
+	    }
+
+	    private readonly string initialConsoleOutput = $@">>Welcome to Calculator!
+>>Enter a mathematical expression:
+>>Ex: 2 x 3";
+
+	    private readonly string endConsoleOutput = $@">>Type 'n' to end program, type new expression to continue.
+<<n";
+    }
+}
